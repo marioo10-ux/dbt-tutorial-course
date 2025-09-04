d1691b6299ad5d9a26dc795ffed85855cc12074d
Mario Rodrigo <mariorodrigo2004@gmail.com>
2025-09-04 20:44:36 +0200

    j

--- lessons/models/marts/orders.sql
+++ lessons/models/marts/orders.sql
@@ -32,3 +32,5 @@ SELECT
 FROM {{ ref('stg_ecommerce__orders') }} AS od
 LEFT JOIN order_item_measures AS om
 	ON od.order_id = om.order_id
+LEFT JOIN {{ ref('int_ecommerce__first_order_created') }} AS fo
+	ON od.user_id = fo.user_id
